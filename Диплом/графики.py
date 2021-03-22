c216825c39e792116dcba414835678885fd77f92
ts-glob <tsoygleb2@gmail.com>
2021-03-22 22:08:33 +0400

    3 фикс

--- Диплом/графики.py
+++ Диплом/графики.py
@@ -4,7 +4,8 @@ import numpy as np
 from skimage import io
 from skimage.color import rgb2gray
 from skimage import img_as_ubyte
-from math import pow, pi, exp, log
+from math import pow, pi, exp, log, fabs
+from cmath import exp as cexp
 import locale
 from skimage.util import random_noise
 
@@ -27,6 +28,7 @@ T = 1  # можно брать единичкой для расчетов
 d = 0.5  # от 0 до T^2
 rho = 0.9  # можно поиграться в пределах от 0 до 1
 Dx = np.var(img)
+Dx = 1
 Dv = np.var(noisy_img)
 omegas = np.arange(N * L) * (2 * pi / (T * N * L - 1)) - pi / T
 _T = T / L
@@ -38,17 +40,18 @@ def H(omega):
 
 
 def Фx(omega):
-    return (-2 * (log(rho) / T) * Dx) / ((log(rho) / T) * (log(rho) / T) + (omega * omega))
+    return (-2 * (log(rho) * Dx / T)) / ((log(rho) / T) * (log(rho) / T) + (omega * omega))
 
 
 # тут вычисляем значение ЧХ в точке, бесконечную сумму не считаем, считаем конечную, но большую
 def G(omega):
     upper = 0
-    lower = L * T * Dv
-    temp = (N * L * 2) - (N * L)
+    lower = L * _T * Dv
+    temp = (N * L)
     for k in range(N * L * 2):
         upper = upper + H(-omega - 2 * pi * (k - temp) / T) * Фx(omega + 2 * pi * (k - temp) / T)
-        lower = lower + pow(H(omega + 2 * pi * (k - temp) / (_T * L)), 2) * Фx(omega + 2 * pi * (k - temp) / (_T * L))
+        lower = lower + pow(fabs(H(omega + 2 * pi * (k - temp) / (_T * L))), 2) * Фx(
+            omega + 2 * pi * (k - temp) / (_T * L))
     return L * upper / lower
 
 
@@ -64,17 +67,19 @@ def G_array():
 
 # тут вычисляем обратное Фурье, точно считать его нам не надо
 def g(Gs):
-    gs = np.zeros(N * L)
-    fx = np.zeros(N * L)
-    step = omegas[1] - omegas[0]
-    for k in range(omegas.shape[0]):
-        fx[k] = Gs[k] * exp(omegas[k] * _T * (k - omegas.shape[0] / 2))
-    for k in range(Gs.shape[0] - 1):
-        gs[k + 1] = gs[k] + ((fx[k] + fx[k + 1]) * step) / 2  # TODO
-    gs = gs * (_T / (2 * pi))
-    for k in range(gs.shape[0]):
+    gs = [complex(0, 0)] * (N * L)
+    i = complex(0, 1)
+    dw = omegas[1] - omegas[0]
+    for k in range(omegas.shape[0] - 1):
+        for w in range(omegas.shape[0] - 1):
+            e1 = cexp(i * complex((omegas[w] * _T * (k - omegas.shape[0] / 2)), 0))
+            e2 = cexp(i * complex((omegas[w + 1] * _T * (k + 1 - omegas.shape[0] / 2)), 0))
+            gs[k] += (Gs[k] * e1 + Gs[k + 1] * e2) * dw / 2
+    for k in range(len(gs)):
+        gs[k] = gs[k] * (_T / (2 * pi))
+        # print(locale.format("%.8f", gs[k].real))
+        print(locale.format("%.8f", gs[k].imag))
         # print(locale.format("%.8f", k - omegas.shape[0] / 2))
-        print(locale.format("%.8f", gs[k]))
     return gs
 
 

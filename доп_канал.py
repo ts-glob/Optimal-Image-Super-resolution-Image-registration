0256f1903d603d1bf63aef97d7f36d8c53b67c4b
ts-glob <tsoygleb2@gmail.com>
2021-05-19 20:12:32 +0400

    GUI 1.8 (fix 1)

--- доп_канал.py
+++ доп_канал.py
@@ -14,7 +14,53 @@ from skimage import io
 from skimage import img_as_ubyte, img_as_float
 from skimage.color import rgb2gray
 import numpy as np
-from math import log
+from math import log, exp, fabs
+
+
+# def additional_channel_gui(files, expand_by, progress_bar_info):
+#     progress_step = 100 / len(files)
+#     progress_bar_info[0]['value'] = 0
+#     progress_bar_info[1].config(text="0")
+#     progress_bar_info[2].update_idletasks()
+#     lil_array = [[[0 for l in range(expand_by)] for ll in range(expand_by)] for lll in range(len(files))]
+#     result_array = [[[0 for l in range(files[0].shape[1])] for ll in range(files[0].shape[0])] for lll in
+#                     range(len(files))]
+#     T = 1
+#     L = expand_by
+#     rho = 0.9
+#     A = -log(rho)
+#     for i in range(0, len(files)):
+#         Dx = np.var(img_as_float(rgb2gray(files[i])))
+#         for a in range(0, L):
+#             for b in range(0, L):
+#                 B = 2 * a * a * b * b - 4 * a * b * (a + b) + (a * a + 6 * a * b + b * b) - a - b
+#                 C = (a * b) * (a * b)
+#                 additional_channel_pixel = 2 * Dx * A * B - 2 * Dx * C
+#                 lil_array[i][a][b] = additional_channel_pixel
+#     for i in tqdm(range(0, len(files)), desc="Доп канал ошибки интерполяции: "):
+#         temp_a = 0
+#         for a in range(0, files[0].shape[0]):
+#             temp_b = 0
+#             if a % L == 1:
+#                 temp_a += L
+#             for b in range(0, files[0].shape[1]):
+#                 result_array[i][a][b] = lil_array[i][a - temp_a][b - temp_b]
+#                 if b % L == 1:
+#                     temp_b += L
+#         progress_bar_info[0]['value'] += progress_step
+#         progress_bar_info[1].config(text=round(progress_bar_info[0]['value']))
+#         progress_bar_info[2].update_idletasks()
+#     progress_bar_info[0]['value'] = 100
+#     progress_bar_info[1].config(text=progress_bar_info[0]['value'])
+#     progress_bar_info[2].update_idletasks()
+#     return result_array
+
+def B(Dx, m, n):
+    m = fabs(m)
+    n = fabs(n)
+    rho = 0.9
+    A = -log(rho)
+    return Dx * exp(-A * (m + n))
 
 
 def additional_channel_gui(files, expand_by, progress_bar_info):
@@ -27,16 +73,22 @@ def additional_channel_gui(files, expand_by, progress_bar_info):
                     range(len(files))]
     T = 1
     L = expand_by
-    rho = 0.9
-    A = -log(rho)
+
     for i in range(0, len(files)):
-        Dx = np.var(img_as_float(rgb2gray(files[i])))
-        for a in range(1, L+1):
-            for b in range(1, L+1):
-                B = 2 * a * a * b * b - 4 * a * b * (a + b) + (a * a + 6 * a * b + b * b) - a - b
-                C = (a * b) * (a * b)
-                additional_channel = 2 * Dx * A * B - 2 * Dx * C
-                lil_array[i][a-1][b-1] = additional_channel
+        Dx = np.var(img_as_ubyte(rgb2gray(files[i])))
+        for a in range(0, L):
+            for b in range(0, L):
+                a0 = 1 - (a / T) - (b / T) + ((a * b) / (T * T))
+                a1 = b / T - ((a * b) / (T * T))
+                a2 = a / T - ((a * b) / (T * T))
+                a3 = ((a * b) / (T * T))
+                additional_channel_pixel = (1 / (L * L)) * (
+                        a0 * a0 * B(Dx, 0, 0) + a1 * a1 * B(Dx, 0, 0) + a2 * a2 * B(Dx, 0, 0) + a3 * a3 * B(Dx, 0, 0) + Dx +
+                        2 * a0 * a1 * B(Dx, 0, T) + 2 * a0 * a2 * B(Dx, T, 0) + 2 * a0 * a3 * B(Dx, T, T) - 2 * a0 * B(Dx, a - 0, b - 0) +
+                        2 * a1 * a2 * B(Dx, T, -T) + 2 * a1 * a3 * B(Dx, T, 0) - 2 * a1 * B(Dx, a, b - T) +
+                        2 * a2 * a3 * B(Dx, 0, T) - 2 * a2 * B(Dx, a - T, b) -
+                        2 * a3 * B(Dx, a - T, b - T))
+                lil_array[i][a][b] = additional_channel_pixel
     for i in tqdm(range(0, len(files)), desc="Доп канал ошибки интерполяции: "):
         temp_a = 0
         for a in range(0, files[0].shape[0]):
--- комплексирование.py
+++ комплексирование.py
@@ -63,19 +63,24 @@ def fusing_gui(files, additional_channel, progress_bar_info):
     progress_bar_info[1].config(text="0")
     progress_bar_info[2].update_idletasks()
     pixel_matrix = [[0 for l in range(files[0].shape[1])] for ll in range(files[0].shape[0])]
+    temp_count2 = 0
     for i in tqdm(range(0, files[0].shape[0]), desc="Комплексирование: "):
         for j in range(0, files[0].shape[1]):
             a = 0
             b = 0
             for m in range(0, len(files)):
-                img = img_as_ubyte(rgb2gray(files[m]))
-                D_trans = additional_channel[m][i][j]
-                if D_trans != 0:
-                    a += img[i][j] / D_trans
-                    b += 1 / D_trans
+                offset_img = img_as_ubyte(rgb2gray(files[m]))
+                disp_err = additional_channel[m][i][j]
+                if disp_err != 0:
+                    a += offset_img[i][j] / disp_err
+                    b += 1 / disp_err
+                else:
+                    a += offset_img[i][j] / 0.0000000001
+                    b += 1 / 0.0000000001
             try:
                 c = a / b
                 if c > 255 or c < 0:
+                    temp_count2 += 1
                     c = 122 #todo
             except:
                 c = files[0][i][j]
@@ -83,6 +88,7 @@ def fusing_gui(files, additional_channel, progress_bar_info):
             progress_bar_info[0]['value'] += progress_step
             progress_bar_info[1].config(text=round(progress_bar_info[0]['value']))
             progress_bar_info[2].update_idletasks()
+    print(temp_count2)
     pixel_matrix = img_as_ubyte((pixel_matrix - np.min(pixel_matrix)) / (np.max(pixel_matrix) - np.min(pixel_matrix)))
     progress_bar_info[0]['value'] = 100
     progress_bar_info[1].config(text=progress_bar_info[0]['value'])

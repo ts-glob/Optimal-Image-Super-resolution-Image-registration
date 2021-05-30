b9429c14ae5806d03cde8b743a73d76b3d908e27
ts-glob <tsoygleb2@gmail.com>
2021-05-30 18:47:32 +0400

    Автоматизация 1.6

--- Автоматизация/Невзвешенное комплексирование/automated_mse.py
+++ Автоматизация/Невзвешенное комплексирование/automated_mse.py
@@ -3,6 +3,7 @@ from skimage import img_as_ubyte
 from skimage.color import rgb2gray
 from skimage import io
 import imageio
+import cv2
 
 
 def sko():
@@ -14,7 +15,9 @@ def sko():
     sko_arr = sko_arr / (np.var(img_as_ubyte(rgb2gray(orig_img))) ** 2)
     string = str(sko_arr).replace(".", ",")
     print("СКО невзвешенного суммирования относительно искаженного видео составляет", string)
+
     weighted_img = img_as_ubyte(rgb2gray(io.imread(weighted)))
+    weighted_img = cv2.resize(weighted_img, (orig_img.shape[1], orig_img.shape[0]))
     sko_arr = np.sum((weighted_img.astype("float") - orig_img.astype("float")) ** 2)
     sko_arr /= float(orig_img.shape[0] * orig_img.shape[1])
     sko_arr = sko_arr / (np.var(img_as_ubyte(rgb2gray(orig_img))) ** 2)
@@ -22,10 +25,10 @@ def sko():
     print("СКО взвешенного суммирования относительно искаженного видео составляет", string)
 
 
-num = '2'
-orig_vid = "D:/my docs/stud/_ДИПЛОМНАЯ РАБОТА/Test Video/" + num + ".gif"
+file_num = '5'
+orig_vid = "D:/my docs/stud/_ДИПЛОМНАЯ РАБОТА/Test Video/" + file_num + ".gif"
 unweighted = "D:\\my docs\\stud\\_ДИПЛОМНАЯ РАБОТА\\Автоматизация\\Невзвешенное комплексирование\\" \
-             "невзвешенное комплексирование\\восстановление в 4 раза\\" + num + ".png"
+             "невзвешенное комплексирование\\восстановление в 8 раз\\" + file_num + ".png"
 weighted = "D:\\my docs\\stud\\_ДИПЛОМНАЯ РАБОТА\\Автоматизация\\" \
-           "Оптимальное комплексирование\\восстановление в 4 раза\\" + num + ".png"
+           "Оптимальное комплексирование\\восстановление в 8 раз\\" + file_num + ".png"
 sko()
